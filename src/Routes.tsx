243a1a847f5e41c9844262a46086744168f5e94a
patryk-polanski <pat.polanski@gmail.com>
2025-10-16 20:22:04 +0200

    feat: add ErrorPage for non-existent routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
 import MoviesPage from "./pages/MoviesPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => (
+  <>
+    <h1>Page not found</h1>
+    <Link to="/">Back home</Link>
+  </>
+);
+
+export default ErrorPage;
